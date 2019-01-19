cf2a9e179ee9da3155e48b1b671628b72b360c49
asmx1986 <sebastianm.alonso@gmail.com>
2019-01-19 11:35:13 -0300

    added share image taken from camera

--- my-drawer-ng/src/app/home/home.component.ts
+++ my-drawer-ng/src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import * as imageSourceModule from "image-source";
 import * as camera from "nativescript-camera";
+import * as SocialShare from "nativescript-social-share";
 import { RadSideDrawer } from "nativescript-ui-sidedrawer";
 import * as app from "tns-core-modules/application";
 import { Image } from "tns-core-modules/ui/image";
@@ -33,6 +35,12 @@ export class HomeComponent implements OnInit {
                         console.log("Tamaño: " + imageAsset.options.width + "x" + imageAsset.options.height);
                         console.log("keepAspectRatio: " + imageAsset.options.keepAspectRatio);
                         console.log("Foto guardada!");
+                        imageSourceModule.fromAsset(imageAsset)
+                            .then((imageSource) => {
+                                SocialShare.shareImage(imageSource, "Asunto: compartido desde el curso!");
+                            }).catch((err) => {
+                                console.log("Error -> " + err.message);
+                            });
                     }).catch((err) => {
                         console.log("Error -> " + err.message);
                     });
